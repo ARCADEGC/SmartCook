0ab8436071892ed6f477492c51f5ad716dabece7
Jaromír Trnka <JrTrnka55@gmail.com>
2024-02-09 15:18:47 +0100

    fixed keep state of theme

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -4,7 +4,7 @@ zoomies.register();
 
 export default function Loader() {
     return (
-        <div className="bg-page justifybg-center flex min-h-[100svh] w-full items-center">
+        <div className="flex min-h-screen w-full items-center justify-center bg-background">
             <l-zoomies size="80" stroke="5" bg-opacity="0.1" speed="1.4" color="black"></l-zoomies>
         </div>
     );
